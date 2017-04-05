29cf9ccf722831e7db1fcdf6f0ee4ceb463ff92d
Juliano <juliano@nationphone.com>
2017-04-05 17:51:34 -0300

    UPDATE - componetes front-end react

--- frontend/src/afazer/afazer.jsx
+++ frontend/src/afazer/afazer.jsx
@@ -4,11 +4,25 @@ import Afazerform from './afazerform'
 import Afazerlist from './afazerlist'
 
 export default class Afazer extends Component{
+    constructor(props){
+        super(props)
+        this.state={description: '', list: []}
+        this.handleChange=this.handleChange.bind(this)
+        this.handleAdd=this.handleAdd.bind(this)
+    }
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+    handleAdd(){
+        console.log(this.state.description)
+    }
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <Afazerform/>
+                <Afazerform description={this.state.description}
+                            handleChange={this.handleChange}
+                            handleAdd={this.handleAdd} />
                 <Afazerlist/>
             </div>
         )
