c4ea8116f706bfb2d8a54145c5450c0174594774
Juliano <juliano@nationphone.com>
2017-04-07 23:39:28 -0300

    UPDATE - Metodos de criar listar e deletar

--- frontend/src/afazer/afazer.jsx
+++ frontend/src/afazer/afazer.jsx
@@ -1,20 +1,39 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import Afazerform from './afazerform'
 import Afazerlist from './afazerlist'
+
+const URL='http://localhost:3003/api/afazeres'
 
 export default class Afazer extends Component{
     constructor(props){
         super(props)
         this.state={description: '', list: []}
+
         this.handleChange=this.handleChange.bind(this)
         this.handleAdd=this.handleAdd.bind(this)
+        this.handleRemove=this.handleRemove.bind(this)
+        this.refresh()
+
     }
+    refresh(){
+        axios.get(`${URL}?sort=-createAt`)
+        .then(resp => this.setState({...this.state, description: '',list: resp.data}))
+    }
+
     handleChange(e){
-        this.setState({...this.state, description: e.target.value})
+        this.setState({...this.state, description: e.target.value })
     }
     handleAdd(){
-        console.log(this.state.description)
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp=>this.refresh())
+    }
+    handleRemove(afazer){
+        axios.delete(`${URL}/${afazer._id}`)
+            .then(resp=>this.refresh())
     }
     render(){
         return(
@@ -23,7 +42,8 @@ export default class Afazer extends Component{
                 <Afazerform description={this.state.description}
                             handleChange={this.handleChange}
                             handleAdd={this.handleAdd} />
-                <Afazerlist/>
+                <Afazerlist list={this.state.list} 
+                            handleRemove={this.handleRemove}/>
             </div>
         )
     }
--- frontend/src/afazer/afazerlist.jsx
+++ frontend/src/afazer/afazerlist.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default prop=>(
- <h1>A fazer lista</h1>
-)
+export default props=>{
+
+    const renderRows = ()=>{
+        const list = props.list || []
+
+        return list.map(afazer => (
+            <tr key={afazer._id}>
+                <td>{afazer.description}</td>
+                <td>
+                    <IconButton style="danger" icon="trash-o"
+                     onClick={()=>props.handleRemove(afazer)}></IconButton>
+                </td>
+            </tr>
+        ))
+    }
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
